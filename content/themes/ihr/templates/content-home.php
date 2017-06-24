ba06632ebcc9ce075d4a61af2243ffc6e3bb0768
mzanni <mikezanni@gmail.com>
2017-06-24 02:03:56 -0700

    home desktop layout

--- content/themes/ihr/templates/content-home.php
+++ content/themes/ihr/templates/content-home.php
@@ -25,9 +25,12 @@ if( have_rows('feature_pages') ): ?>
 <div class="feature-pages">
 
 	<?php
-    while ( have_rows('feature_pages') ) : the_row(); ?>
+	$i=1;
+		# code...
+    while ( have_rows('feature_pages') ) : the_row();    
+    ?>
 
-		<div class="feature">
+		<div class="feature<?php echo ($i%2 == 0 ? ' right' : '');?>">
 
 			<?php
 			$image = get_sub_field('image');
@@ -42,13 +45,14 @@ if( have_rows('feature_pages') ): ?>
 			<div class="copy">
 
 				<h2><?php the_sub_field('title'); ?></h2>
-				<p><?php the_sub_field('excerpt'); ?></p>
-				<a class="read-more button" href="<?php the_sub_field('url'); ?>"<?php echo (get_sub_field('new_window') ? ' target="_blank"' : ''); ?>><?php the_sub_field('cta_text'); ?></a>
+				<p><?php the_sub_field('excerpt'); ?> test</p>
 			</div>
+			<a class="read-more button" href="<?php the_sub_field('url'); ?>"<?php echo (get_sub_field('new_window') ? ' target="_blank"' : ''); ?>><?php the_sub_field('cta_text'); ?></a>
 
 		</div>
 
 	<?php
+	$i++;
     endwhile;
 	?>
 
@@ -57,6 +61,79 @@ if( have_rows('feature_pages') ): ?>
 <?php
 endif;
 ?>
+
+
+
+
+
+<?php
+$post_objects = get_field('feature_posts');
+
+if( $post_objects ): ?>
+
+<div class="featured-blog-posts">
+
+	<h3 class="bar">featured blog posts</h3>
+
+    <?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
+        <?php setup_postdata($post); ?>
+        
+        <article class="entry-container">
+
+	        <?php the_post_thumbnail('thumbnail'); ?>
+
+	        <div class="entry-copy">
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+				<?php
+
+					
+					$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+					
+
+					$time_string = sprintf( $time_string,
+						get_the_date( DATE_W3C ),
+						get_the_date()
+					);
+
+
+					$categories = get_the_category();
+					$separator = ' ';
+					$category_list = '';
+					if ( ! empty( $categories ) ) {
+					    foreach( $categories as $category ) {
+					        //$category_list .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+					        $category_list .= '<span>' . esc_html( $category->name ) . '</span>' . $separator;
+					    }
+					}
+
+				?>
+
+				<div class="entry-meta">
+					<span class="cat"><?php echo trim( $category_list, $separator ); ?></span> <span class="pipe">|</span> 
+					<span class="byline"><span class="author vcard"><?php echo get_the_author(); ?></span></span> <span class="pipe">|</span> 
+					<?php echo $time_string; 
+					//<time class="entry-date published" datetime="2008-10-17T04:33:51+00:00">October 17, 2008</time>?>
+				</div>
+			</div>
+
+		</article>
+
+    <?php endforeach; ?>
+
+    <a class="read-more" href="/blog/">View all blog posts</a>
+    
+    </div>
+
+
+    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+<?php endif;
+?>
+
+
+
+
+
 
 
 
--- content/themes/ihr/templates/footer.php
+++ content/themes/ihr/templates/footer.php
@@ -1,5 +1,73 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+
+<div class="newsletter-signup">
+<div class="wrap">
+	<div class="container">
+
+		<div class="heading">
+			<h3>Join our newsletter</h3>
+
+			<p>Keep up with the latest Internet Health news.</p>
+		</div>
+
+
+		<form id="newsletter_form" class="signup-form" name="newsletter_form" action="https://www.mozilla.org/en-US/newsletter/" method="post">
+			<input type="hidden" id="fmt" name="fmt" value="H">
+			<input type="hidden" id="lang" name="lang" value="en">
+			<input type="hidden" id="newsletters" name="newsletters" value="internet-health-report-group">
+
+			<div id="newsletter_errors" class="newsletter_errors"></div>
+
+			<div id="newsletter_email" class="form_group form_group_email">
+				<input type="email" id="email" name="email" class="signup-form__email" required="" placeholder="Your email here" size="30">
+				<button id="newsletter_submit" type="submit" class="signup-form__btn">Join us</button>
+			</div>
+
+			<div id="newsletter_privacy" class="form_group form_group-agree">
+				<label for="privacy">
+				<input type="checkbox" id="privacy" name="privacy" class="signup-form__checkbox" required="">
+				<div class="signup-form__checkbox-label">I’m okay with mozilla handling my info as explained in this <a href="https://www.mozilla.org/privacy/websites/">privacy notice</a>.
+				</div>
+				</label>
+			</div>
+
+			<div>
+			</div>
+
+		</form>
+
+
+
+
+	</div>
+
+</div>
+</div>
+
+
+<div class="footer-content">
+<div class="wrap">
+	<div class="container">
+
+		<div class="link-list">
+
+			<a href=""><span class="icon-mail"></span>Email</a>
+			<a href=""><span class="icon-cc"></span>License</a>
+			<a href=""><span class="icon-conduct"></span>Code of Conduct</a>
+			<a href=""><span class="icon-legal"></span>Legal</a>
+			<a href=""><span class="icon-privacy"></span>Privacy</a>
+			<a href=""><span class="icon-cookies"></span>Cookies</a>
+
+		</div>
+
+
+		<a class="logo" href="http://mozilla.org"></a>
+
+		<div class="statement">We are a global non-profit dedicated to putting you in control of your online experience and shaping the future of the web for the public good. Visit us at <a href="http://mozilla.org">mozilla.org</a>
+
+	</div>
+</div>
+</div>
+
+
 </footer>
